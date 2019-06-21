fa3281b31f886a7952b5c8b158f300557ea29a57
hdlguy <hdlguy@hotmail.com>
2019-06-21 17:28:55 -0400

    starting to simulate the whole sat_chan

--- fpga/source/gps_synthesizer/sat_chan.sv
+++ fpga/source/gps_synthesizer/sat_chan.sv
@@ -1,8 +1,23 @@
 module sat_chan (
     input  logic        clk,
+    input  logic        enable,
     input  logic[31:0]  freq,
     input  logic[15:0]  gain,
-    output logic[15:0]  sout
+    output logic[15:0]  real_out,
+    output logic[15:0]  imag_out
 );
+
+    logic [8:0] nco_real, nco_imag;    
+    logic [15:0] scaled_nco_real, scaled_nco_imag;
+    doppler_nco doppler_nco_inst ( .clk(clk), .enable(enable), .freq(freq), .real_out(nco_real), .imag_out(nco_imag) );
+
+    // we need to scale the imaginary part of the NCO output by 1/sqrt(2) ~ 0.7
+    localparam int p_scale = (2.0**16)/$sqrt(2.0);
+    always_ff @(posedge clk) begin
+        scaled_nco_real <= {nco_real, 7'b0000000};
+        scaled_nco_imag <= p_scale; 
+        
+    end
+
 
 endmodule
---
+++ fpga/source/gps_synthesizer/sat_chan_tb.sv
@@ -0,0 +1,27 @@
+`timescale 1 ns / 1 ps
+
+module sat_chan_tb ();
+
+    logic        enable;
+    logic[31:0]  freq;
+    logic[15:0]  gain;
+    logic[15:0]  real_out;
+    logic[15:0]  imag_out;
+
+
+    localparam clk_period = 10;
+    logic clk = 0;
+    always #(clk_period/2) clk = ~clk;
+    
+    sat_chan uut(.*);
+  
+    initial begin
+        freq = 32'h01234567;
+        enable = 0;
+        #(clk_period*10);
+        enable = 1;
+        #(clk_period*1000);
+        freq = 32'h02468ace;
+     end
+
+endmodule
