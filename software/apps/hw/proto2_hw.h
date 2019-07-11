d8861c60e1c1460ee70db0f6182c36460df98c27
pedro <pete@hdlguy.com>
2019-07-11 20:52:52 +0000

    register definitions in header file.

--- software/apps/hw/proto2_hw.h
+++ software/apps/hw/proto2_hw.h
@@ -1,11 +1,22 @@
-#define		BASE_ADDRESS		0x40000000
-#define		PROTO_SIZE		0x04000000
+#define     BASE_ADDRESS        0x40000000
+#define     PROTO_SIZE          0x04000000
 
-#define 	REGFILE_OFFSET		0x00000000
+#define     REGFILE_OFFSET      0x00000000
 
-#define 	FPGA_ID     		REGFILE_OFFSET+0x04*0  // Currently returns 0xDEADBEEF
-#define 	FPGA_VERSION		REGFILE_OFFSET+0x04*1  // Returns major and minor version numbers.
+#define     TEST_BRAM_OFFSET    0x00010000
+#define     TEST_BRAM_SIZE      0x00001000 // 4KB
+
+#define     FPGA_ID             REGFILE_OFFSET+0x04*0  // Currently returns 0xDEADBEEF
+#define     FPGA_VERSION        REGFILE_OFFSET+0x04*1  // Returns major and minor version numbers.
     
-#define         TEST_BRAM_OFFSET    0x00010000
-#define         TEST_BRAM_SIZE      0x00001000 // 4KB
+// The registers of the GPS emulator.
+#define     EMU_NOISE_GAIN      REGFILE_OFFSET+0x04*7  // Noise level of combined satellite signals.
+
+#define     N_SAT               4
+#define     EMU_REG_START       8
+#define     EMU_REG_STEP        4
+// These are the registers within the set for each satelite
+#define     EMU_DOPP_FREQ       0 // bits[31:0] = (2^32)*Fdop/Fs where Fdop is the doppler frequency desired and Fs is the sampling rate of the system.
+#define     EMU_DOPP_GAIN       1 // bits[15:0] = gain in 1.15 unsigned format. Ie., 0xffff ~ 1.0;
+#define     EMU_DOPP_CA_SEL     2 // bits[5L0] = SV Gold Code
 
